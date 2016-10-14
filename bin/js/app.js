c36e4de7025af06774f7e6e4d24b207770f08e52
Sean McGrath <Sean McGrath>
2016-10-14 20:16:26 +0100

    added angular-route through bower

--- bin/js/app.js
+++ bin/js/app.js
@@ -1,9 +1,18 @@
-var GoDropBox = angular.module('GoDropBox', []);
+var GoDropBox = angular.module('GoDropBox', [ngRoute]);
 
+
+GoDropBox.config(function($routeProvider){
+    $routeProvider.
+        when('/',{
+            templateUrl: 'pages/login.html',
+            controller: 'homeController'
+        });
+
+});
 
 //main page controller
 GoDropBox.controller('homeController', ['$scope', function($scope){
-
+    console.log($scope);
 }]);
 
 GoDropBox.controller('loginController', ['$scope', function($scope){
