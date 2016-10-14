d5c80a837d924049a0a4267c62f3c13cf57a6f1e
Sean McGrath <Sean McGrath>
2016-10-14 19:17:09 +0100

    changed the file structure and sorted style sheet links

--- bin/js/app.js
+++ bin/js/app.js
@@ -1,1 +1,11 @@
 var GoDropBox = angular.module('GoDropBox', []);
+
+
+//main page controller
+GoDropBox.controller('homeController', ['$scope', function($scope){
+
+}]);
+
+GoDropBox.controller('loginController', ['$scope', function($scope){
+
+}]);
