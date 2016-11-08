360d4451ccd257648297d21db9027fde298d7d9c
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2016-11-08 18:46:52 +0000

    go get github.com/go-martini/martini - No longer maintained

--- src/main/main.go
+++ src/main/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"../github.com/gorilla/mux"
+	//"log"
+	//"net/http"
+	//"../github.com/gorilla/mux"
+	"../github.com/go-martini/martini"
 )
 
 func main() {
+	/*
 	r := mux.NewRouter()
 	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("public/"))))
 
 	log.Println("Listening at port 8080")
 	http.ListenAndServe(":8080", r)
+	*/
+
+	m := martini.Classic()
+
+
+	m.Run()
 }
