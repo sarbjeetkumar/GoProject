b9e1e9c796d061cb040eaad9b38e01d739207dc9
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2016-11-09 19:41:50 +0000

    Register user with post

--- src/main/main.go
+++ src/main/main.go
@@ -1,23 +1,51 @@
 package main
 
 import (
-	//"log"
-	//"net/http"
-	//"../github.com/gorilla/mux"
 	"../github.com/go-martini/martini"
+	"../github.com/martini-contrib/render"
+	"../github.com/martini-contrib/binding"
+	"gopkg.in/mgo.v2"
 )
 
-func main() {
-	/*
-	r := mux.NewRouter()
-	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("public/"))))
+type Register struct {
+	Name  		string `form:"name"`
+	Email 		string `form:"email"`
+	Username 	string `form:"username"`
+	Password 	string `form:"password"`
+	Age 		string `form:"age"`
+	Sex 		string `form:"sex"`
+}
 
-	log.Println("Listening at port 8080")
-	http.ListenAndServe(":8080", r)
-	*/
+func DB() martini.Handler {
+	session, err := mgo.Dial("localhost:27017/users") // mongodb://localhost
+	if err != nil {
+		panic(err)
+	}
+
+	return func(c martini.Context) {
+		s := session.Clone()
+		c.Map(s.DB("users")) // local
+		defer s.Close()
+		c.Next()
+	}
+}
+
+func main() {
 
 	m := martini.Classic()
 
+	m.Use(render.Renderer(render.Options {
+		Directory: "public/pages", // Specify what path to load the templates from.
+		//Extensions: []string{".tmpl", ".html"}, // Specify extensions to load for templates.
 
-	m.Run()
+
+	}))
+	m.Use(DB())
+
+	m.Post("/", binding.Form(Register{}), func(register Register, r render.Render, db *mgo.Database) {
+		db.C("users").Insert(register)
+	})
+
+	//m.Run() uses the default port 3000
+	m.RunOnAddr(":8080")//specifies port 8080
 }
