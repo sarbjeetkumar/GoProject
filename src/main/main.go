e9490de36a1fcc6b5e646dec91f946ea7854bda4
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2016-11-10 17:36:17 +0000

    Error check POST for unique email

--- src/main/main.go
+++ src/main/main.go
@@ -5,6 +5,9 @@ import (
 	"../github.com/martini-contrib/render"
 	"../github.com/martini-contrib/binding"
 	"gopkg.in/mgo.v2"
+	//"gopkg.in/mgo.v2/bson"
+	"fmt"
+	"log"
 )
 
 type Register struct {
@@ -17,16 +20,40 @@ type Register struct {
 }
 
 func DB() martini.Handler {
-	session, err := mgo.Dial("localhost:27017/users") // mongodb://localhost
+	session, err := mgo.Dial("localhost:27017") // mongodb://localhost
 	if err != nil {
 		panic(err)
 	}
 
+	emailIndex(session)
+
 	return func(c martini.Context) {
 		s := session.Clone()
-		c.Map(s.DB("users")) // local
+		//Database GoDropBox
+		c.Map(s.DB("GoDropBox")) // local
 		defer s.Close()
 		c.Next()
+	}
+}
+
+
+func emailIndex(s *mgo.Session) {
+	//Modified from - http://goinbigdata.com/how-to-build-microservice-with-mongodb-in-golang/
+	session := s.Copy()
+	defer session.Close()
+
+	c := session.DB("GoDropBox").C("users")
+
+	index := mgo.Index{
+		Key: []string{"email"},
+		Unique: true,
+		DropDups: true,
+		Background: true,
+		Sparse: true,
+	}
+	err := c.EnsureIndex(index)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -35,15 +62,21 @@ func main() {
 	m := martini.Classic()
 
 	m.Use(render.Renderer(render.Options {
-		Directory: "public/pages", // Specify what path to load the templates from.
-		//Extensions: []string{".tmpl", ".html"}, // Specify extensions to load for templates.
-
-
+		Directory: "public/pages",
 	}))
+
 	m.Use(DB())
 
 	m.Post("/", binding.Form(Register{}), func(register Register, r render.Render, db *mgo.Database) {
-		db.C("users").Insert(register)
+
+		if err := db.C("users").Insert(register); err != nil {
+
+			if mgo.IsDup(err) {
+				fmt.Printf("%s Already exsists ", register.Email)
+				// Is a duplicate key, but we don't know which one
+			}
+		}
+
 	})
 
 	//m.Run() uses the default port 3000
