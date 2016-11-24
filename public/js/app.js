23eb1e676cc3df765849687e2ba48c188185a0db
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2016-11-24 15:47:45 +0000

    Changes

--- public/js/app.js
+++ public/js/app.js
@@ -92,26 +92,17 @@ GoDropBox.config(function($routeProvider){
             .when('/signin',{
                         templateUrl: 'pages/login.html',
                         controller: 'loginController'
-                    })
+            })
             .when('/signup',{
                 templateUrl: 'pages/register.html',
-<<<<<<< HEAD
                 controller: 'loginController'
-            })
-           
-=======
-                controller: 'homeController'
-            })
-            .when('/about',{
-                templateUrl: 'pages/about.html',
-                controller: 'homeController'
-            })
+           })
              .when('/home',{
                         templateUrl: 'pages/dragAndDrop.html',
                         controller: 'homeController'
                     });
 
->>>>>>> 5a4a3884f1a940d5c60968ee23026320d663293c
+
 });
 
 //main page controller
