f59b17e568be2a75365ca607e64cba347aa4dd36
Sean McGrath <Sean McGrath>
2016-11-25 13:19:10 +0000

    Added changes

--- public/js/app.js
+++ public/js/app.js
@@ -78,12 +78,12 @@ Directive is returned after ng-view renders
 GoDropBox.config(function($routeProvider){
     $routeProvider
            .when('/',{
-                 templateUrl: 'pages/dragAndDrop.html',
+                templateUrl: 'pages/dragAndDrop.html',
                 controller: 'homeController'
             })
            .when('/signin',{
-                        templateUrl: 'pages/login.html',
-                        controller: 'loginController'
+                templateUrl: 'pages/login.html',
+                controller: 'loginController'
             })
            .when('/signup',{
                 templateUrl: 'pages/register.html',
